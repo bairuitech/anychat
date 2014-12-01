9cfbb2f5d8dacb06aa6b683f2609932f3b6cc73a
xingzewen102 <feiwudexue2008@qq.com>
2014-12-01 17:34:53 +0800

    配置分辨率修改

--- android/src/HelloAnyChat/src/com/example/helloanychat/MainActivity.java
+++ android/src/HelloAnyChat/src/com/example/helloanychat/MainActivity.java
@@ -78,6 +78,7 @@ public class MainActivity extends Activity implements AnyChatBaseEvent {
 		// 初始化登陆配置数据
 		initLoginConfig();
 		initWaitingTips();
+		ApplyVideoConfig();
 		// 注册广播
 		registerBoradcastReceiver();
 	}
