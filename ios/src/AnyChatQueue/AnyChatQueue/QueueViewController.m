5af4417f7ee292175fd06c8efecc4ed25a9bab04
bairuitech <wangxl@bairuitech.com>
2015-07-15 21:26:26 +0800

    简化时间转换函数

--- ios/src/AnyChatQueue/AnyChatQueue/QueueViewController.m
+++ ios/src/AnyChatQueue/AnyChatQueue/QueueViewController.m
@@ -176,29 +176,7 @@
 
 // 时间转换
 - (NSString *)transform:(int)second {
-    int h = 0;
-    int d = 0;
-    int s = 0;
-    int temp = second % 3600;
-    if (second > 3600) {
-        h = second / 3600;
-        if (temp != 0) {
-            if (temp > 60) {
-                d = temp / 60;
-                if (temp % 60 != 0) {
-                    s = temp % 60;
-                }
-            } else {
-                s = temp;
-            }
-        }
-    } else {
-        d = second / 60;
-        if (second % 60 != 0) {
-            s = second % 60;
-        }
-    }
-    return [NSString stringWithFormat:@"%d时 %d分 %d秒",h,d,s];
+    return [NSString stringWithFormat:@"%d时 %d分 %d秒", second/(60*60), (second%(60*60))/60, second%60];
 }
 
 #pragma mark - Memory
