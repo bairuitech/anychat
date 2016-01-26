dfd21b076ee31138eec0a1a75837f4b86f117a18
tonywoo2012 <40137139@qq.com>
2016-01-26 17:15:28 +0800

    提交C++的应用签名库，增加Linux下的库文件，在库内将AppID转换为大写

--- tools/anychatsign/src/c++/anychatsign/anychatsign.cpp
+++ tools/anychatsign/src/c++/anychatsign/anychatsign.cpp
@@ -4,7 +4,20 @@
 #include <string>
 #include "libanychatsign.h"
 
-
+// 命令参数描述与示例
+void usage(void)
+{
+	printf("demo for rsa sign or verify. Usage:\r\n");
+	printf("\tanychatsign [options]\r\n");
+	printf("Option:\r\n");
+	printf("\t-h  help, print this message.\r\n");
+	printf("\t-m  mode, 0 for rsa sign mode; 1 for rsa verify mode.\r\n");
+	printf("\t-u  useid, sdk userid.\r\n");
+	printf("\t-a  appid, application guid.\r\n");
+	printf("\t-k  keyfile, rsa privateFile or publicFile.\r\n");
+	printf("\t-s  sigfile, rsa sign data file for read or write.\r\n");
+	printf("\t-t  timestamp, the timestamp by sign.\r\n");
+}
 
 int main(int argc, char* argv[])
 {
@@ -15,7 +28,7 @@ int main(int argc, char* argv[])
 	int timestamp = 0;
 	std::string appid;
 	std::string keyfile;
-	std::string sigstr;
+	std::string sigfile;
 	while( (result = getopt(argc, argv, "hm:u:a:k:s:t:")) != -1 )
 	{
 		switch(result)
@@ -25,16 +38,16 @@ int main(int argc, char* argv[])
 		case 'u':		userid = atoi(optarg);		break;
 		case 'a':		appid = optarg;				break;
 		case 'k':		keyfile = optarg;			break;
-		case 's':		sigstr = optarg;			break;
+		case 's':		sigfile= optarg;			break;
 		case 't':		timestamp = atoi(optarg);	break;
 		default:
 			break;
 		}
 	}
-	if(help)
+	if(help || argc<2)
 	{
 		// Show usage
-		printf("anychatsign -h\r\n");
+		usage();
 		return 0;
 	}
 	if(!keyfile.length())
@@ -42,9 +55,9 @@ int main(int argc, char* argv[])
 		printf("must input keyfile!\r\n");
 		return 0;
 	}
-	if(!mode && !sigstr.length())
+	if(mode==1 && !sigfile.length())
 	{
-		printf("rsa verify mode, must input sig string!\r\n");
+		printf("rsa verify mode, must input sign file!\r\n");
 		return 0;
 	}
 
@@ -75,11 +88,43 @@ int main(int argc, char* argv[])
 		char szSigStr[1024]= {0};
 		timestamp = 0;
 		int errorcode = AnyChatRsaSign(userid, appid.c_str(), szKeyBuf, szSigStr, sizeof(szSigStr), timestamp);
-		printf("RSA sign result, errorcode:%d, \r\n\ttimestamp:%d, \r\n\tsigstr:%s\r\n", errorcode, timestamp, sigstr);
+		printf("RSA sign result, errorcode:%d, \r\n\ttimestamp:%d, \r\n\tsigstr:%s\r\n", errorcode, timestamp, szSigStr);
+
+		if (sigfile.length()){
+			FILE* fp = fopen(sigfile.c_str(), "wb");
+			if (fp){
+				fwrite(szSigStr, sizeof(char), strlen(szSigStr), fp);
+				fclose(fp);
+				fp = NULL;
+				printf("write sigfile:%s success!\r\n", sigfile.c_str());
+			}else{
+				printf("write sigfile:%s error!\r\n", sigfile.c_str());
+			}
+		}
 	}
 	else
 	{
-		int errorcode = AnyChatRsaVerify(userid, appid.c_str(), sigstr.c_str(), timestamp, szKeyBuf);
+		char szSignBuf[1024] = {0};
+		FILE* fp = NULL;
+		do 
+		{
+			fp = fopen(sigfile.c_str(), "rb");
+			if(!fp)
+				break;
+			fread(szSignBuf, 1, sizeof(szSignBuf)-1, fp);
+		} while (false);
+		if(fp)
+		{
+			fclose(fp);
+			fp = NULL;
+		}
+		if(!strlen(szSignBuf))
+		{
+			printf("read sign file:%s failed!\r\n", sigfile.c_str());
+			return 0;
+		}
+
+		int errorcode = AnyChatRsaVerify(userid, appid.c_str(), szSignBuf, timestamp, szKeyBuf);
 		printf("RSA verify result, errorcode:%d", errorcode);
 	}
 	return 0;
