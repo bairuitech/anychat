fb95d5035b8e86163da30c95414ea7f59132042f
tonywoo2012 <40137139@qq.com>
2016-02-01 19:37:14 +0800

    WebDemo示例程序增加签名登录功能实现。

--- web/src/AnyChatWebDemo/javascript/logicfunc.js
+++ web/src/AnyChatWebDemo/javascript/logicfunc.js
@@ -95,17 +95,42 @@ function InitInterfaceUI() {
     }
     //登录按钮
     GetID("loginbtn").onclick = function () {
+    	var signTimestamp = 0;
+    	var signStr = "";
+    	
         if (GetID("password").value == "密码可为空")
             GetID("password").value = "";
         if (GetID("username").value != "") {
             DisplayLoadingDiv(true);
             setLoginInfo();
-            if (GetID("AppGuid") && GetID("AppGuid").value.length)				// 设置应用ID
-                BRAC_SetSDKOption(BRAC_SO_CLOUD_APPGUID, GetID("AppGuid").value.toString());
+            if (GetID("normal_login").checked){
+            	if (GetID("AppGuid") && GetID("AppGuid").value.length)				// 设置应用ID
+                	BRAC_SetSDKOption(BRAC_SO_CLOUD_APPGUID, GetID("AppGuid").value.toString());
+            }
+            if (GetID("sign_login").checked){
+                if (GetID("AppGuid") && GetID("AppGuid").value.length){
+                	jsonObj = getSign("http://192.168.5.112:8980/", -1, GetID("username").value, GetID("AppGuid").value);
+                	if (jsonObj != null){
+                		if (jsonObj.errorcode == 0){
+                			signStr = jsonObj.sigStr;
+                			signTimestamp = jsonObj.timestamp;
+                		}                		
+                	}
+                }
+            }
+            
             var errorcode = BRAC_Connect(GetID("ServerAddr").value, parseInt(GetID("ServerPort").value)); //连接服务器
             AddLog("BRAC_Connect(" + GetID("ServerAddr").value + "," + GetID("ServerPort").value + ")=" + errorcode, LOG_TYPE_API);
-            errorcode = BRAC_Login(GetID("username").value, GetID("password").value, 0);
-            AddLog("BRAC_Login(" + GetID("username").value + ")=" + errorcode, LOG_TYPE_API);
+            
+            if (GetID("normal_login").checked){
+				errorcode = BRAC_Login(GetID("username").value, GetID("password").value, 0);
+            	AddLog("BRAC_Login(" + GetID("username").value + ")=" + errorcode, LOG_TYPE_API);            	
+            }
+            if (GetID("sign_login").checked){
+            	errorcode = BRAC_LoginEx(GetID("username").value, -1, GetID("username").value, GetID("AppGuid").value, signTimestamp, signStr, "");
+            	AddLog("BRAC_LoginEx(" + GetID("username").value + ")=" + errorcode, LOG_TYPE_API);
+            }
+            
             // 隐藏设置界面
             GetID("setting_div").style.display = "none";
 
@@ -492,10 +517,18 @@ function reVideoDivSize() {
 
 //设置登录信息，包括用户名、服务器IP、服务器端口、应用ID
 function setLoginInfo() {
+	var loginType = 0;
     setCookie('username',GetID("username").value,30);
     setCookie('ServerAddr',GetID("ServerAddr").value,30);
     setCookie('ServerPort',GetID("ServerPort").value,30);   
     setCookie('AppGuid',GetID("AppGuid").value,30);
+    if (GetID("normal_login").checked){
+    	loginType = GetID("normal_login").value;
+    }
+    if (GetID("sign_login").checked){
+    	loginType = GetID("sign_login").value;
+    }
+    setCookie('loginType',loginType,30);
 }
 
 //获取登录信息
@@ -508,6 +541,13 @@ function getLoginInfo() {
     if (serverPort != "")
         GetID("ServerPort").value = serverPort;
     GetID("AppGuid").value = getCookie("AppGuid");
+    var loginType = getCookie("loginType");
+    if (loginType == GetID("normal_login").value){
+    	GetID("normal_login").checked = true;
+    }
+	if (loginType == GetID("sign_login").value){
+    	GetID("sign_login").checked = true;
+    }    
 }
 
 //获取cookie项的cookie值
@@ -529,4 +569,71 @@ function setCookie(c_name, value, expiredays){
 　　var exdate=new Date();
 　　exdate.setDate(exdate.getDate() + expiredays);
 　　document.cookie=c_name+ "=" + value + ((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+}
+
+//去除字符串两边空格
+String.prototype.trim = function () {
+    return this.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+//获取签名
+function getSign(signUrl, userId, strUser, appId) {
+	var retVal = null;
+	var parameter = "userid=" + userId + "&struserid=" + strUser + "&appid=" + appId ;
+    var requestURL = signUrl;
+    xmlHttpReq = createXMLHttpRequest();
+        xmlHttpReq.open("POST",requestURL,true);
+        xmlHttpReq.setRequestHeader("Content-Type","application/x-www-form-urlencoded;");
+        xmlHttpReq.send(encodeURI(encodeURI(parameter)));
+    
+    xmlHttpReq.onreadystatechange = function(){
+        if(xmlHttpReq.readyState == 4){
+            switch(xmlHttpReq.status){
+                case 200:
+                    responseText = xmlHttpReq.responseText;
+                    retVal = JSON.parse(responseText);
+                    break;
+                case 400:
+                    alert("错误的请求！\nError Code:400!");
+                    break;
+                case 403:
+                    alert("拒绝请求！\nError Code:403!");
+                    break;
+                case 404:
+                    alert("请求地址不存在！\nError Code:404!");
+                    break;
+                case 500:
+                    alert("内部错误！\nError Code:500!");
+                    break;
+                case 503:
+                    alert("服务不可用！\nError Code:503!");
+                    break;
+                default:
+                    alert("请求返回异常！\nError Code:"+xmlHttpReq.status);
+                    break;
+            }
+        }
+    }
+    
+    return retVal;
+}
+
+// 创建XMLHttpRequest对象
+function createXMLHttpRequest() {
+    if (window.XMLHttpRequest) {// IE 7.0及以上版本和非IE的浏览器
+        xmlHttpReq = new XMLHttpRequest();
+    } else {// IE 6.0及以下版本
+        try {
+            xmlHttpReq = new ActiveXObject("MSXML2.XMLHTTP");
+        }catch (e) {
+            try {
+                xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
+            }catch (e) {}
+        }
+    }
+    if (!xmlHttpReq) {
+        alert("当前浏览器不支持!");
+        return null;
+    }
+    return xmlHttpReq;
 }
