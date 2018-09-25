b20f4b3f90bc54a8e89f5b1b131a02d0b56f9f0c
yangjc <yangjiachao8855@163.com>
2018-09-25 14:39:09 +0800

    去掉一些重复定义的消息常量
    Change-Id: I89a66739d85b0908485c57f01fabfb456f59d053

--- web/src/AnyChatQueue/js/anychatsdk.js
+++ web/src/AnyChatQueue/js/anychatsdk.js
@@ -286,15 +286,6 @@ var WM_GV_CAMERASTATE		=	WM_GV + 11;		// 用户摄像头状态发生变化，wParam（INT）表
 var WM_GV_CHATMODECHG		=	WM_GV + 12;		// 用户聊天模式发生变化，wParam（INT）表示用户ID号，lParam（INT）表示用户的当前聊天模式
 var WM_GV_ACTIVESTATE		=	WM_GV + 13;		// 用户活动状态发生变化，wParam（INT）表示用户ID号，lParam（INT）表示用户的当前活动状态
 var WM_GV_P2PCONNECTSTATE	=	WM_GV + 14;		// 本地用户与其它用户的P2P网络连接状态发生变化，wParam（INT）表示其它用户ID号，lParam（INT）表示本地用户与其它用户的当前P2P网络连接状态
-var WM_GV_PRIVATEREQUEST	=	WM_GV + 21;		// 用户发起私聊请求，wParam（INT）表示发起者的用户ID号，lParam（INT）表示私聊请求编号，标识该请求
-var WM_GV_PRIVATEECHO		=	WM_GV + 22;		// 用户回复私聊请求，wParam（INT）表示回复者的用户ID号，lParam（INT）为出错代码
-var WM_GV_PRIVATEEXIT		=	WM_GV + 23;		// 用户退出私聊，wParam（INT）表示退出者的用户ID号，lParam（INT）为出错代码
-var WM_GV_VIDEOFULLSCREEN	=	WM_GV + 32;		// 用户视频全屏通知，wParam（INT）表示用户ID，lParam(DWORD)表示当前视频是否全屏显示
-var WM_GV_SDKWARNING      	=   WM_GV + 41;		// SDK警告信息，当SDK在运行过程中自检发现异常状态时，将向上层发送该消息，wParam（INT）表示警告代码，定义为：GV_ERR_WARNING_XXXX
-var WM_GV_USERINFOUPDATE	=	WM_GV + 16;		// 用户信息更新通知，wParam（INT）表示用户ID号，lParam（INT）表示更新类别
-var WM_GV_FRIENDSTATUS		=	WM_GV + 17;		// 好友在线状态变化，wParam（INT）表示好友用户ID号，lParam（INT）表示用户的当前活动状态：0 离线， 1 上线
-var WM_GV_VIDEOSIZECHG		=	WM_GV + 15;		// 用户视频分辩率发生变化，wParam（INT）表示用户ID号，lParam（INT）表示用户的视频分辨率组合值（低16位表示宽度，高16位表示高度）
-
 var WM_GV_VIDEOSIZECHG		=	WM_GV + 15;		// 用户视频分辩率发生变化，wParam（INT）表示用户ID号，lParam（INT）表示用户的视频分辨率组合值（低16位表示宽度，高16位表示高度）
 var WM_GV_USERINFOUPDATE	=	WM_GV + 16;		// 用户信息更新通知，wParam（INT）表示用户ID号，lParam（INT）表示更新类别
 var WM_GV_FRIENDSTATUS		=	WM_GV + 17;		// 好友在线状态变化，wParam（INT）表示好友用户ID号，lParam（INT）表示用户的当前活动状态：0 离线， 1 上线
@@ -309,7 +300,7 @@ var WM_GV_SERVERCFGUPDATE	=	WM_GV + 24;		// 服务器配置参数更新
 var WM_GV_EXTENDBTNPRESS	=	WM_GV + 31;		// 用户按下扩展按钮，wParam（INT）表示按钮所对应的用户ID号，lParam（DWORD）指示按钮（左下角）所在屏幕位置(x,y)，用户可以利用该参数显示菜单等
 var WM_GV_VIDEOFULLSCREEN	=	WM_GV + 32;		// 用户视频全屏通知，wParam（INT）表示用户ID，lParam(DWORD)表示当前视频是否全屏显示
 
-var WM_GV_SDKWARNING		=	WM_GV + 41;		// SDK警告信息，当SDK在运行过程中自检发现异常状态时，将向上层发送该消息，wParam（INT）表示警告代码，定义为：GV_ERR_WARNING_XXXX
+var WM_GV_SDKWARNING      	=   WM_GV + 41;		// SDK警告信息，当SDK在运行过程中自检发现异常状态时，将向上层发送该消息，wParam（INT）表示警告代码，定义为：GV_ERR_WARNING_XXXX
 
 var WM_GV_AUDIOPLAYCTRL		=	WM_GV + 100;	// 音频播放设备控制，wParam:1 打开播放器， 0 关闭播放器， lParam：profile，android平台内部使用
 var WM_GV_AUDIORECCTRL		=	WM_GV + 101;	// 音频录音设备控制，wParam:1 打开音频采集设备，0 关闭音频采集设备
