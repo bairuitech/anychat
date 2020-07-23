aa76d7aea3fedc19cf3c83a7299e4d38eadd94ba
bairuitech <wangxl@bairuitech.com>
2020-07-23 21:05:25 +0800

    删除无用工程

--- tools/anychat-sdk-jar/src/com/bairuitech/anychat/AnyChatObjectEvent.java
+++
@@ -1,6 +0,0 @@
-package com.bairuitech.anychat;
-
-// AnyChat业务对象事件通知接口
-public interface AnyChatObjectEvent {
-    public void OnAnyChatObjectEvent(int dwObjectType, int dwObjectId, int dwEventType, int dwParam1, int dwParam2, int dwParam3, int dwParam4, String strParam);
-}
--- tools/anychat-sdk-jar/src/com/bairuitech/anychat/AnyChatStreamCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.bairuitech.anychat;
-
-// AnyChat媒体数据回调接口
-public interface AnyChatStreamCallBack {
-	// 视频数据回调接口
-    public void OnAnyChatVideoDataCallBack(int userid, int streamindex, byte[] buf, int len, int width, int height);
-    // 音频数据回调接口
-    public void OnAnyChatAudioDataCallBack(int userid, int streamindex, byte[] buf, int len, int timestamp, int channels, int samplespersecond, int bitspersample);
-}
